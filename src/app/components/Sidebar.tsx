e1ef0134e9e9f96d0ebd3c640c5d86cbabe25bf3
modernscape <modernscape@gmail.com>
2025-10-18 00:07:15 +0900

    25-10-18_

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -4,16 +4,17 @@ import { useThingStore } from "../store/thingsStore";
 
 export default function Sidebar() {
   const style_btn = "flex items-center m-2 gap-2";
+  const view = useThingStore((state) => state.view);
   const setView = useThingStore((state) => state.setView);
 
   return (
     <div className="flex pr-4 text-2xl">
       <div className="flex flex-col">
-        <button onClick={() => setView("active")} className={style_btn}>
+        <button onClick={() => setView("active")} className={style_btn} style={{ fontWeight: view === "active" ? "bold" : "normal" }}>
           <Menu />
           One Thing
         </button>
-        <button onClick={() => setView("trashed")} className={style_btn}>
+        <button onClick={() => setView("trashed")} className={style_btn} style={{ fontWeight: view === "trashed" ? "bold" : "normal" }}>
           <Trash />
           ゴミ箱
         </button>
