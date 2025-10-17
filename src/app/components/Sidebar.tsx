f2b6ad25b07e39ad21c2b6fda251294d45deeda0
modernscape <modernscape@gmail.com>
2025-10-18 00:01:38 +0900

    25-10-18_

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,13 +1,23 @@
-import { Trash } from "lucide-react";
+"use client";
+import { Menu, Trash } from "lucide-react";
+import { useThingStore } from "../store/thingsStore";
 
 export default function Sidebar() {
+  const style_btn = "flex items-center m-2 gap-2";
+  const setView = useThingStore((state) => state.setView);
+
   return (
-    <div>
-      <div>
-        <button>One Thing</button>
-        <button>ゴミ箱</button>
+    <div className="flex pr-4 text-2xl">
+      <div className="flex flex-col">
+        <button onClick={() => setView("active")} className={style_btn}>
+          <Menu />
+          One Thing
+        </button>
+        <button onClick={() => setView("trashed")} className={style_btn}>
+          <Trash />
+          ゴミ箱
+        </button>
       </div>
-      <main>ThingsList</main>
     </div>
   );
 }
--- src/app/components/ThingList.tsx
+++ src/app/components/ThingList.tsx
@@ -1,3 +1,7 @@
+"use client";
+import { useThingStore } from "../store/thingsStore";
+
 export default function ThingList() {
-  return <div>dummy</div>;
+  const view = useThingStore((state) => state.view);
+  return view === "active" ? <div>things</div> : <div>trash</div>;
 }
