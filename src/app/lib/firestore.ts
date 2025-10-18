502aea8f067a50aa5f5b76a7ac9eb5c4311a8c17
modernscape <modernscape@gmail.com>
2025-10-19 00:39:06 +0900

    25-10-19_

--- src/app/lib/firestore.ts
+++ src/app/lib/firestore.ts
@@ -1,29 +1,48 @@
 import { db } from "./firebase";
-import { collection, addDoc } from "firebase/firestore";
+import { collection, doc, addDoc, updateDoc, deleteDoc, query, orderBy, onSnapshot } from "firebase/firestore";
 import { Thing } from "@/types";
 
 const key_things = "things";
-/// Thing
+
+/*
+Thing
+*/
 // Create
 export async function addThing(title: string, order: number) {
-  const ref = collection(db, key_things); // things
-  return await addDoc(ref, {
+  const ref = collection(db, key_things);
+  return addDoc(ref, {
+    id: new Date().toString(),
     title,
+    order,
     items: [],
     trashed: false,
-    order: order,
     createdAt: new Date(),
   });
 }
-// Read
-export async function subscribe() {}
 
+// Read
+export async function subscribe(callback: (things: Thing[]) => void) {
+  const q = query(collection(db, key_things), orderBy("order", "asc"));
+  return onSnapshot(q, (snapshot) => {
+    const things_ = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() } as Thing));
+    callback(things);
+  });
+}
 // Update
-export async function updateThing(id: string, newData: Partial<Thing>) {}
+export async function updateThing(id: string, newData: Partial<Thing>) {
+  const thingRef = doc(db, key_things, id);
+  await updateDoc(thingRef, newData);
+}
 
 // Delete
+export async function deleteThing(id: string) {
+  const thingRef = doc(db, key_things, id);
+  await deleteDoc(thingRef);
+}
 
-/// Item
+/*
+Item
+*/
 // Create
 // Read
 // Update
