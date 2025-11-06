da3a3660f6f852ddcd6bbab55349c69f5139fa3a
modernscape <modernscape@gmail.com>
2025-11-06 15:18:39 +0900

    25-11-06_

--- src/app/components/ItemList.tsx
+++ src/app/components/ItemList.tsx
@@ -1,24 +1,44 @@
 import { Thing, Item } from "@/types"
 import { deleteItem, updateItemAtIndex, addItem } from "../lib/firestore";
+import { useEffect, useRef } from "react";
 
 export function ItemList({ thing }: { thing: Thing }) {
     const items = thing.items as Item[];
+
+    const inputRefs = useRef<HTMLInputElement[]>([])
+    const prevCount = useRef(items.length)
+
+    useEffect(() => {
+        if (items.length > prevCount.current) {
+            const lastIndex = items.length - 1
+            inputRefs.current[lastIndex]?.focus()
+        }
+        prevCount.current = items.length
+    }, [items.length])
+
     return (
         <div>
             <ul className="flex flex-wrap text-[22px] my-4 gap-x-4 text-[#277a4f]">
                 {items.map((item, i) =>
                     <li key={item.id} >
-                        <input type="text" defaultValue={item.text} style={{ width: "200px" }} placeholder="item" onKeyDown={(e) => {
-                            if (e.key !== "Enter") return
-                            const inputEl = e.target as HTMLInputElement
-                            const text = inputEl.value
-                            if (text === "") {
-                                deleteItem(thing.id, item.id)
-                            } else {
-                                updateItemAtIndex(thing.id, i, inputEl.value)
-                                inputEl.blur()
-                            }
-                        }} />
+                        <input type="text"
+                            defaultValue={item.text}
+                            style={{ width: "200px" }}
+                            placeholder="item"
+                            onKeyDown={(e) => {
+                                if (e.key !== "Enter") return
+                                const inputEl = e.target as HTMLInputElement
+                                const text = inputEl.value
+                                if (text === "") {
+                                    deleteItem(thing.id, item.id)
+                                } else {
+                                    updateItemAtIndex(thing.id, i, inputEl.value)
+                                    inputEl.blur()
+                                }
+                            }}
+                            ref={(el) => {
+                                if (el) inputRefs.current[i] = el
+                            }} />
                     </li>
                 )}
                 <li>
