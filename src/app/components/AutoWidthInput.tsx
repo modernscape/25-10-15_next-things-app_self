cc1983c31d2b79564ce0f1e1cec9da102737805c
modernscape <modernscape@gmail.com>
2025-11-09 19:57:42 +0900

    25-11-09_

--- src/app/components/AutoWidthInput.tsx
+++ src/app/components/AutoWidthInput.tsx
@@ -1,8 +1,14 @@
 import { useState, useRef, useLayoutEffect } from "react"
 
-export default function AutoWidthInput({ value: initial }: { value: string }) {
-
-
+export default function AutoWidthInput({
+    value: initial,
+    onConfirm,
+    font,
+}: {
+    value: string,
+    onConfirm: (val: string) => void,
+    font?: string
+}) {
     const [val, setVal] = useState(initial) // inputの文字
     const spanRef = useRef<HTMLSpanElement>(null) // ダミーのspan
     const [w, setW] = useState(1) // 幅
@@ -13,5 +19,30 @@ export default function AutoWidthInput({ value: initial }: { value: string }) {
         }
     }, [val])
 
-    return <div></div>
+    return (
+        <div>
+            <span
+                ref={spanRef}
+                style={{
+                    position: "absolute",
+                    visibility: "hidden",
+                    whiteSpace: "pre",
+                    font: font || "inheret"
+                }}
+            >{val || " "}</span>
+            <input
+                type="text"
+                value={val}
+                style={{ width: w, font: font || "inherit" }}
+                onChange={(e) => setVal(e.target.value)}
+                onKeyDown={(e) => {
+                    const event = e.nativeEvent as KeyboardEvent;
+                    if (e.key === "Enter" && !event.isComposing) {
+                        onConfirm(val)
+                        e.currentTarget.blur();
+                    }
+                }}
+            />
+        </div>
+    )
 }
